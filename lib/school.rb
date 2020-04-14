23bdf0dc672e2b8d8a5ab9b58dc7b7a4a06db3df
Jack Cooper <jackcooper1245@gmail.com>
2020-04-14 15:04:06 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School 
+  
+  attr_accessor:name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+def roster(roster)
+@roster = roster
+end
+
+def roster
+  @roster = {}
+end
+
+
+def add_student(name, grade)
+  if !@roster[grade]
+    @roster[grade] = [name]
+  else
+    @roster[grade] << name
+  end
+end
+
+end
